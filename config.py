64d78090997283991c9282f5f480778b8350f7a9
Achraf LOUIZA <achraf.louiza97@gmail.com>
2023-06-07 15:05:39 +0200

    Adding config to embed all global variables

---
+++ config.py
@@ -0,0 +1,11 @@
+# Data processing parameters
+BUFFER_SIZE = 400
+
+# Model parameters
+NUM_LAYERS = 4
+NUM_HEADS = 8
+MODEL_DIMENSION = 128
+DROPOUT_PROBABILITY = 0.1
+VOCAB_SIZE = 5000
+# Training parameters
+BATCH_SIZE = 64
